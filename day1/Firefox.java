bd12a2eb84cbe26745b50c75c9148c2167993445
Gokul-10gv <gokulofficial.gv@gmail.com>
2025-06-27 11:48:11 +0530

    Add files via upload
    week1

---
+++ day1/Firefox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Firefox {
+public static void main(String[] args) {
+	float browserVersion = 100.2f;
+	String browserName = "firefox";
+	boolean isVisible = true;
+	int releaseYear = 1998;
+	char browserLogo = 'f';
+	System.out.println("Browser version:" + browserVersion); 
+	System.out.println("Browser Name:" + browserName);
+	System.out.println("isVisible:" +isVisible);
+	System.out.println("releaseYear:" + releaseYear);
+	System.out.println("browserLogo:" + browserLogo);
+	
+		
+			
+			
+	
+	
+			
+}
+}
+
