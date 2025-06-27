b5d6595d91712c905fd4ed5a6a5ee5a984abc308
Gokul-10gv <gokulofficial.gv@gmail.com>
2025-06-27 18:30:46 +0530

    Add files via upload

---
+++ day1/day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 39.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseyear = 2008;
+		char shortcutKey = 'c';
+		System.out.println("version:" + version + "\n" + "developer:" + developer + "\n" + "isBeta:" + isBeta + "\n"
+				+ "releaseyear:" + releaseyear + "\n" + "shortcutKey:" + shortcutKey);
+
+	}
+
+}
